f83a278dd9a7e24994054d43a447c9e94a1ebc92
helton <helton.ribeiro@enalta.com>
2017-07-26 16:20:03 -0300

    Version 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-esper/clj-esper "1.0.2-SNAPSHOT" 
+(defproject clj-esper/clj-esper "1.0.2" 
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.espertech/esper "6.1.0" :exclusions [log4j]]]
